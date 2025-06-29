e8f6991cda3d391fd59969120e9deb7b922c1224
Rodrigo Vasconcelso <rodrigovmoreira92@gmail.com>
2025-06-28 22:54:17 -0300

    feat: Enhance QR code handling and update index view references

--- server.js
+++ server.js
@@ -15,9 +15,16 @@ function startServer(whatsappClient) {
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, 'views'));
 
+  let lastQr = null;
+
   // Manipulador de QR Code
-  whatsappClient.on('qr', async (qr) => {
-    console.log('🔁 Atualizando QR Code...');
+ whatsappClient.on('qr', (qr) => {
+    lastQr = qr;
+    generateAndEmitQr(io, qr);
+  });
+
+  // Função para gerar e emitir o QR Code
+  const generateAndEmitQr = async (io, qr) => {
     try {
       const qrImageUrl = await qrcode.toDataURL(qr);
       io.emit('qr', qrImageUrl);
@@ -25,6 +32,27 @@ function startServer(whatsappClient) {
     } catch (error) {
       console.error('Erro ao gerar QR Code:', error);
     }
+  };
+
+    app.get('/', (req, res) => {
+    res.render('index');
+    if (lastQr) {
+      generateAndEmitQr(io, lastQr);
+    }
+  });
+
+  // Quando um cliente se conecta via Socket.IO
+  io.on('connection', (socket) => {
+    console.log('Novo cliente conectado');
+    
+    // Se já tivermos um QR Code, envia imediatamente para o novo cliente
+    if (lastQr) {
+      generateAndEmitQr(socket, lastQr);
+    }
+
+    socket.on('disconnect', () => {
+      console.log('Cliente desconectado');
+    });
   });
 
   whatsappClient.on('ready', () => {
