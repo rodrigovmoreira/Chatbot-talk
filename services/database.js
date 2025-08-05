939d84b42e3530befe5baccf6416983a2ddc6f6b
rodrigo.vasconcelos <rodrigo.vasconcelos@gpssa.com.br>
2025-08-05 15:33:10 -0300

    feat: Refactor message handling and integrate message model for improved data management

--- services/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('✅ MongoDB conectado'))
-    .catch(err => console.error('❌ Erro MongoDB:', err));
-};
